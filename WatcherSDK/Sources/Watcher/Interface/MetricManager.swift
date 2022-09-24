5ff1e0c9e93c43dcbe25e02253289abf8a10410c
Skwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-09-24 14:02:45 +0200

    🧹 Clean up

--- WatcherSDK/Sources/Watcher/Interface/MetricManager.swift
+++ WatcherSDK/Sources/Watcher/Interface/MetricManager.swift
@@ -8,24 +8,35 @@
 import Foundation
 import Combine
 
+/// An object that manages a metric provider and provides convenience Combine accessors wrapped around it.
 open class MetricManager: MetricManagerUseCase {
     
     // MARK: - Private
     
+    /// The raw metric provider
     private let metricProvider: any MetricProviderUseCase
-    private let metricSubject: CurrentValueSubject<Float, Never> = .init(0)
+    
+    /// The metric subject, used as a `private(set)` accessor
+    private let metricSubject: PassthroughSubject<Float, Never> = .init()
+    
+    /// The metric's threshold events subject, used as a `private(set)` accessor
     private let thresholdEventSubject: CurrentValueSubject<MetricThresholdState, Never> = .init(.nominal)
     
+    /// The metric's threshold range
     internal private(set) var thresholdRange: MetricThresholdRange = .lower
+    
+    /// This manager's background timer, used to repeatedly fetch the metric & publish its values
     private lazy var timer: BackgroundTimer = { makeAndScheduleTimer() }()
+    
+    /// The dispatch queue used by the background timer
     private let queue: DispatchQueue
     
     private func updateTimer() {
         timer = makeAndScheduleTimer()
     }
     
-    private func computeThresholdState() {
-        let metric = metricSubject.value
+    private func computeThresholdState(for metricValue: Float) {
+        let metric = metricValue
         
         let thresholdEvent = thresholdRange.mapToState(metric, threshold: threshold)
         
@@ -38,8 +49,9 @@ open class MetricManager: MetricManagerUseCase {
         let timer = BackgroundTimer(timeInterval: Double(refreshFrequency), queue: queue) { [weak self] in
             guard let self else { return }
             do {
-                try self.metricSubject.send(self.metricProvider.fetchMetric())
-                self.computeThresholdState()
+                let newValue = try self.metricProvider.fetchMetric()
+                self.metricSubject.send(newValue)
+                self.computeThresholdState(for: newValue)
             } catch {
                 // TODO: needs more thorough error handling, but eventually if this fails it's not worth crashing
                 // just handle silently (don't emit, but log)
@@ -52,14 +64,7 @@ open class MetricManager: MetricManagerUseCase {
     
     // MARK: - Internal
     
-    internal func set(threshold: Float, range: MetricThresholdRange) {
-        // set the range first, as threshold events are re-calculated after mutating the threshold
-        self.thresholdRange = range
-        self.threshold = threshold
-    }
-    
-    // MARK: - Public
-    public init(metricProvider: any MetricProviderUseCase,
+    internal init(metricProvider: any MetricProviderUseCase,
                   threshold: Float = 0.5,
                   refreshFrequency: TimeInterval = 1,
                   queue: DispatchQueue) {
@@ -68,6 +73,12 @@ open class MetricManager: MetricManagerUseCase {
         self.refreshFrequency = refreshFrequency
         self.queue = queue
         self.timer.resume()
+    }
+    
+    internal func set(threshold: Float, range: MetricThresholdRange) {
+        // set the range first, as threshold events are re-calculated after mutating the threshold
+        self.thresholdRange = range
+        self.threshold = threshold
     }
     
     // MARK: - Protocol Conformance
