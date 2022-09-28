22bb04255e86b1d6617a712a386417d651dddfab
Skwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-09-28 22:27:12 +0200

    📄 Add code docs on public APIs

--- WatcherSDK/Sources/Watcher/Watcher.swift
+++ WatcherSDK/Sources/Watcher/Watcher.swift
@@ -4,14 +4,17 @@ import OSLog
 
 public class Watcher {
     
+    /// The CPU Load Manager exposes CPU usage through Combine Publishers
     public private(set) var cpuLoadManager: MetricManager
-
+    /// The Memory Load Manager exposes RAM usage through Combine Publishers
     public private(set) var memoryLoadManager: MetricManager
-    
+    /// The Battery Level Manager exposes Battery charge levels through Combine Publishers
     public private(set) var batteryLevelManager: MetricManager
-    
+    /// The Event Provider emits MetricThresholdEvents through Combine Publishers. It subscribes to all 3 metric managers by default (CPU, MEM & BAT).
+    ///
+    /// To only receive events for specific metrics, you can instantiate your own instance and call the `.register(_:)` function with the managers of your choice.
     public private(set) var eventProvider: EventProvider
-
+    
     private init(cpuLoadManager: MetricManager,
                  memoryLoadManager: MetricManager,
                  batteryLevelManager: MetricManager,
@@ -24,6 +27,21 @@ public class Watcher {
         registerBackgroundTasks()
     }
     
+    /// Make sure to call this function in your App's `didFinishLaunching` function, as calling it at any point later will cause a crash.
+    ///
+    /// The Watcher SDK registers Background Tasks, and those need to be registered before `didFinishLaunching` returns.
+    /// Additionally, make sure you add the SDK's Background Task Identifier to your App's Info.plist.
+    ///
+    /// **Background Task Identifier**
+    ///
+    /// `com.datadog.watcher.background-process`
+    ///
+    /// - parameters:
+    ///   - cpuThreshold: Percentage value the CPU Usage should reach before `.critical` ThresholdStates are emitted
+    ///   - memoryLoadThreshold: Percentage value the RAM Usage should reach before `.critical` ThresholdStates are emitted
+    ///   - batteryLevelThreshold: Percentage value the CPU Usage should dip below before `.critical` ThresholdStates are emitted
+    ///   - refreshFrequency: Rate at which metrics should be fetched, in seconds. Lower values require more processing power, use with caution.
+    /// - returns: A fully-qualified Watcher instance, ready to be used to monitor system metrics.
     public static func configure(cpuThreshold: Float = 0.25,
                                  memoryLoadThreshold: Float = 0.01,
                                  batteryLevelThreshold: Float = 0.2,
@@ -91,14 +109,14 @@ private extension Watcher {
         eventProvider.register(cpuLoad, for: .cpu)
         
         let memoryLoadQueue = DispatchQueue(label: "memory-load",
-                                         qos: .background)
+                                            qos: .background)
         let memoryLoad = MemoryLoad(threshold: memoryLoadThreshold,
                                     refreshFrequency: refreshFrequency,
                                     queue: memoryLoadQueue)
         eventProvider.register(memoryLoad, for: .memory)
         
         let batteryLevelQueue = DispatchQueue(label: "battery-level",
-                                            qos: .background)
+                                              qos: .background)
         let batteryLevel = BatteryLevel(threshold: batteryLevelThreshold,
                                         refreshFrequency: refreshFrequency,
                                         queue: batteryLevelQueue)
